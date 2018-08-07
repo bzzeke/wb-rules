f7ef1d2964e9d94ec58f60a9a1499f72939b1fb0
Ilya Shalnev <zeke.home@gmail.com>
2018-08-07 09:33:59 +0000

    [~] Hosts updated

--- scripts/ups.sh
+++ scripts/ups.sh
@@ -2,10 +2,10 @@
 . /etc/wb-rules/scripts/.env
 
 if [ "$1" == "charge" ]; then
-    upsc ups@$SYNOLOGY_HOST | grep battery.charge: | awk '{print $2}'
+    upsc apc@$UPS_HOST | grep battery.charge: | awk '{print $2}'
 elif [ "$1" == "voltage" ]; then
-    upsc ups@$SYNOLOGY_HOST | grep input.voltage: | awk '{print $2}'
+    upsc apc@$UPS_HOST | grep input.voltage: | awk '{print $2}'
 elif [ "$1" == "status" ]; then
-	upsc ups@$SYNOLOGY_HOST | grep "ups.status: "| awk '{print $2}'
+	upsc apc@$UPS_HOST | grep "ups.status: "| awk '{print $2}'
 fi
 

ba66bb4c97ffd45a82f8865d85d0f9af1fe06a11
Ilya Shalnev <zeke.home@gmail.com>
2019-01-25 16:36:11 +0400

    [~] Util: new virtual device to check if wb-rules are alive was added.

--- util.js
+++ util.js
@@ -4,6 +4,10 @@ defineVirtualDevice('util', {
         'Occupancy': {
             type: 'switch',
             value: false
+        },
+        'Alive': {
+            type: 'text',
+            value: ''
         }
     }
 });
@@ -12,5 +16,13 @@ defineRule("_synology_home_mode", {
   whenChanged: "util/Occupancy",
   then: function (newValue, devName, cellName) {
     runShellCommand("/etc/wb-rules/scripts/homemode.py " + ((newValue == 1) ? 'on' : 'off'));
+  }
+});
+
+defineRule("_keep_alive", {
+  when: cron("1 * * * *"),
+  then: function () {
+    var d = new Date();
+    dev.util['Alive'] = Math.floor(d.getTime() / 1000).toString();
   }
 });

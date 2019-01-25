2fcd28816cdb94cf8215dae750cf46e2591b47eb
Ilya Shalnev <zeke.home@gmail.com>
2019-01-25 16:34:40 +0400

    [~] Logic was rewritten from timers to cron.

--- hardnode.js
+++ hardnode.js
@@ -1,5 +1,3 @@
-startTicker("hnTimer", 10000);
-
 defineVirtualDevice('hardnode', {
     title: 'Hardnode',
     cells: {
@@ -18,19 +16,17 @@ defineVirtualDevice('hardnode', {
     }
 });
 
-defineRule("doHnClick", {
-  when: function () {
-    return timers.hnTimer.firing;
-  },
-  then: function () {
-    runShellCommand("/etc/wb-rules/scripts/sensors.py", {
-      captureOutput: true,
-      exitCallback: function (exitCode, capturedOutput) {
-        var data = JSON.parse(capturedOutput);
-		dev['hardnode']['CPU temperature'] = data['cpu_temp'];
-		dev['hardnode']['Fan1 speed'] = data['fans'][0] || 0;
-		dev['hardnode']['Fan2 speed'] = data['fans'][1] || 0;
-      }
-    });	        
-  }
+defineRule("hardnode.status", {
+    when: cron("@every 10s"),
+    then: function () {
+        runShellCommand("/etc/wb-rules/scripts/sensors.py", {
+            captureOutput: true,
+            exitCallback: function (exitCode, capturedOutput) {
+                var data = JSON.parse(capturedOutput);
+                dev['hardnode']['CPU temperature'] = data['cpu_temp'];
+                dev['hardnode']['Fan1 speed'] = data['fans'][0] || 0;
+                dev['hardnode']['Fan2 speed'] = data['fans'][1] || 0;
+            }
+        });
+    }
 });
--- ups.js
+++ ups.js
@@ -1,5 +1,3 @@
-startTicker("clickTimer", 10000);
-
 defineVirtualDevice('ups', {
     title: 'UPS',
     cells: {
@@ -22,25 +20,30 @@ defineVirtualDevice('ups', {
     }
 });
 
-defineRule("doClick", {
-  when: function () {
-    return timers.clickTimer.firing;
-  },
-  then: function () {
-    runShellCommand("/etc/wb-rules/scripts/ups.py", {
-      captureOutput: true,
-      exitCallback: function (exitCode, capturedOutput) {
-        var data = JSON.parse(capturedOutput);
-		dev['ups']['Battery charge'] = data['battery.charge'] || 0;
-		dev['ups']['Input voltage'] = data['input.voltage'] || 0;
-        dev['ups']['Load'] = data['ups.load'] ? data['ups.load'] + '%' : 0;
-        
-        if (data['ups.status']) {
-			dev['ups']['Status'] = (data['ups.status'] == 'OL' || data['ups.status'] == 'OL CHRG' ? 'Online' : (data['ups.status'] == 'OB' ? 'Battery' : 'Low battery'));
-        } else if (data['error']) {
-          	dev['ups']['Status'] = data['error'];
-        }
-      }
-    });	        
-  }
+
+defineRule("ups.status", {
+    when: cron("@every 10s"),
+    then: function () {
+        runShellCommand("/etc/wb-rules/scripts/ups.py", {
+            captureOutput: true,
+            exitCallback: function (exitCode, capturedOutput) {
+                var data = JSON.parse(capturedOutput);
+                dev['ups']['Battery charge'] = data['battery.charge'] || 0;
+                dev['ups']['Input voltage'] = data['input.voltage'] || 0;
+                dev['ups']['Load'] = data['ups.load'] ? data['ups.load'] + '%' : 0;
+
+		if (data['ups.status']) {
+                    if (data['ups.status'] == 'OL' || data['ups.status'] == 'OL CHRG' || data['ups.status'] == 'OL CHRG LB') {
+                        dev['ups']['Status'] = 'Online';
+                    } else if (data['ups.status'] == 'OB' || data['ups.status'] == 'OB DISCHRG') {
+                        dev['ups']['Status'] = 'Battery';
+                    } else {
+                        dev['ups']['Status'] = 'Low battery';
+                  }
+                } else if (data['error']) {
+                    dev['ups']['Status'] = data['error'];
+                }
+            }
+        });
+    }
 });

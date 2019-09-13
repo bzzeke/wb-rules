5479dfcbc540c0059dc6f536a287f79305b0cf5a
Ilya Shalnev <zeke@cs-cart.com>
2019-09-14 00:55:33 +0400

    [~] Dash parser: API extension: /dash and /charts

--- services/modules/dash_parser/dash_parser.py
+++ services/modules/dash_parser/dash_parser.py
@@ -1,19 +1,52 @@
 #!/usr/bin/python3
 
+from socketserver import ThreadingMixIn
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import os
 import util
+import urllib
+
+class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
+    pass
 
 class Server(BaseHTTPRequestHandler):
-    def _set_headers(self):
+
+    def do_GET(self):
+        self.respond()
+
+    def respond(self):
+
+        path = urllib.parse.unquote(self.path).strip("/")
+        path = path.split("/")
+        method = path[0]
+        if hasattr(self, method):
+            del path[0]
+            response = getattr(self, method)(path)
+        else:
+            response = b""
+            self.send_response(404)
+            self.end_headers()
+
+        self.wfile.write(response)
+
+    def dash(self, args):
         self.send_response(200)
         self.send_header("Content-type", "text/json")
         self.end_headers()
 
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write(json.dumps(getwidgets()).encode("utf-8"))
+        return json.dumps(getwidgets()).encode("utf-8")
+
+    def charts(self, args):
+        self.send_response(200)
+        self.send_header("Content-type", "text/json")
+        self.end_headers()
+
+        with open(os.environ["CHARTS_FILE"]) as f:
+            return f.read().encode("utf-8")
+
+        return ""
+
 
 def cell_get_type(cell):
     if cell["type"] in ["temperature", "text", "rel_humidity", "concentration", "voltage", "value", "power", "power_consumption"]:
@@ -118,7 +151,7 @@ def getwidgets():
 
 def main():
     server_address = ("", int(os.environ["DASH_PORT"]))
-    httpd = HTTPServer(server_address, Server)
+    httpd = ThreadingHTTPServer(server_address, Server)
     util.prnt("[dash_parser] Starting...")
     httpd.serve_forever()
 

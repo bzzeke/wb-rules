769a5c500422850a9b34efd950bac39f5711fab1
Ilya Shalnev <zeke@cs-cart.com>
2019-10-06 00:29:52 +0400

    [~] Monitoring: device/host names were added. Ability to check devices by input voltage was added.

--- services/modules/monitoring/monitoring.py
+++ services/modules/monitoring/monitoring.py
@@ -19,20 +19,34 @@ class HostChecker:
     addresses = {}
     sleep = 20
     last_state = {}
-    def __init__(self, addresses):
-        self.addresses = addresses.split(",")
-
+    def __init__(self, prefix):
+        self.addresses = self.parse_hosts(prefix)
         s = threading.Thread(target = self.loop)
         s.daemon = True
         s.start()
 
+    def parse_hosts(self, prefix):
+        it = 0
+        result = {}
+        while "%s_%i" % (prefix, it) in os.environ:
+            name, url = os.environ["%s_%i" % (prefix, it)].split(",")
+            host, port = url.split(":")
+            result[name] = {
+                "host": host,
+                "port": int(port)
+            }
+            it += 1
+
+        return result
+
     @staticmethod
     def tcp(host, port=65533, timeout=2, tries=0):
+
         max_tries = 3
-        s = socket.socket()
-        s.settimeout(timeout)
         result = False
         try:
+            s = socket.socket()
+            s.settimeout(timeout)
             s.connect((host, int(port)))
             s.close()
             result = True
@@ -45,26 +59,26 @@ class HostChecker:
 
         return result
 
-    def notify(self, address, result):
+    def notify(self, name, result):
 
         if result:
-            message = "Host is online: %s" % (address)
+            message = "Host is online: %s" % (name)
         else:
-            message = "Host is down: %s" % (address)
+            message = "Host is down: %s" % (name)
 
         notify(message)
 
     def loop(self):
         while True:
-            for address in self.addresses:
-                host, port = address.split(":")
-                result = HostChecker.tcp(host, port)
+            for name in self.addresses:
 
-                if address in self.last_state and self.last_state[address] != result:
-                    self.notify(address, result)
+                result = HostChecker.tcp(self.addresses[name]["host"], self.addresses[name]["port"])
 
-                self.last_state[address] = result
-                publish(PUBLISH_TOPIC_HOSTS % address, result)
+                if name in self.last_state and self.last_state[name] != result:
+                    self.notify(name, result)
+
+                self.last_state[name] = result
+                publish(PUBLISH_TOPIC_HOSTS % name, result)
 
             time.sleep(self.sleep)
 
@@ -74,24 +88,46 @@ class DeviceChecker:
     sleep = 20
     tdiff = 20
     last_state = {}
+    last_voltage = {}
 
-    def __init__(self, devices):
-        self.set_devices(devices)
+    def __init__(self, prefix):
+        self.devices = self.parse_devices(prefix)
 
         s = threading.Thread(target = self.loop)
         s.daemon = True
         s.start()
 
-    def set_devices(self, devices):
-        for device in devices.split(","):
-            dev, control = device.split(":")
-            self.devices[dev] = control
+    def parse_devices(self, prefix):
+        it = 0
+        result = {}
+
+        while "%s_%i" % (prefix, it) in os.environ:
+            name, url = os.environ["%s_%i" % (prefix, it)].split(",")
+
+            device, controls = url.split(":")
+            if ("|" in controls):
+                control, voltage = controls.split("|")
+            else:
+                control = controls
+                voltage = ""
+
+            result[device] = {
+                "name": name,
+                "control": control,
+                "voltage": voltage
+            }
+            it += 1
+
+        return result
 
     def on_connect(self, client, userdata, rc):
         if rc != 0:
             return
 
         client.subscribe(SUBSCRIBE_TOPIC)
+        for device in self.devices:
+            if self.devices[device]["voltage"]:
+                client.subscribe("/devices/%s/controls/%s" % (device, self.devices[device]["voltage"]))
 
     def on_message(self, client, userdata, msg):
 
@@ -99,11 +135,16 @@ class DeviceChecker:
         device = parts[2]
         control = parts[4]
 
-        if device in self.devices and self.devices[device] == control:
-            if msg.payload == b"r":
+
+        if device in self.devices:
+            if self.devices[device]["control"] == control:
+                if msg.payload == b"r":
+                    self.state[device] = time.time()
+                elif device in self.state:
+                    del self.state[device]
+            elif self.devices[device]["voltage"] == control:
                 self.state[device] = time.time()
-            elif device in self.state:
-                del self.state[device]
+
 
     def notify(self, device, result):
 
@@ -129,11 +170,11 @@ class DeviceChecker:
             for key, value in devs.items():
 
                 if key in self.last_state and self.last_state[key] != value:
-                    self.notify(key, value)
+                    self.notify(self.devices[key]["name"], value)
 
                 self.last_state[key] = value
 
-                publish(PUBLISH_TOPIC_DEVICES % key, value)
+                publish(PUBLISH_TOPIC_DEVICES % self.devices[key]["name"], value)
 
             time.sleep(self.sleep)
 
@@ -143,8 +184,8 @@ def publish(topic, result):
 
 def main():
     try:
-        hc = HostChecker(os.environ["HOST_CHECKER"])
-        dc = DeviceChecker(os.environ["DEVICE_CHECKER"])
+        hc = HostChecker("HOST_CHECKER")
+        dc = DeviceChecker("DEVICE_CHECKER")
 
         client.on_connect = dc.on_connect
         client.on_message = dc.on_message
@@ -154,6 +195,6 @@ def main():
         while True:
             rc = client.loop()
             if rc != 0:
-                break
+               break
     except Exception as e:
         sys.exit(1)
